0db3d7826bae158a033df66b5276cea5657f201b
Ryan <kodegomd3p.ryan.malacao@gmail.com>
2023-02-04 11:02:49 +0800

    first commit

---
+++ app/src/main/java/com/android/sample/repo/Data.kt
@@ -0,0 +1,60 @@
+package com.android.sample.repo
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.LocalDate
+import kotlin.random.Random
+
+//@RequiresApi(Build.VERSION_CODES.O)
+//fun main(){
+//   val dateSample = LocalDate.now()
+//    println(dateSample)
+ //   val randomSample = Random.nextInt(0,100)
+  //  println(randomSample)
+//}
+
+fun main(){
+    var int1 = 4
+    println("Enter number: ")
+    var int2 = readln().toInt()
+
+    var addedTotal = int1 + int2
+    /*var subTotal = int1 - int2
+    var multiTotal = int1 * int2
+    var divTotal = int1 / int2
+    var modTotal = int1 % int2*/
+
+    println(addedTotal)
+    /*println(subTotal)
+    println(multiTotal)
+    println(divTotal)
+    println(modTotal)*/
+
+    when (addedTotal) {
+        6 -> {
+            println("CONDITION 1 = $addedTotal")
+        }
+        7 -> {
+            println("CONDITION 2 = $addedTotal")
+        }
+        else -> {
+            println("CONDITION 3 = $addedTotal")
+        }
+    }
+
+   /* val booleanSample = true
+    val booleanSample2 = false
+
+    println(booleanSample)
+    println(booleanSample2)
+    println(!booleanSample)
+    println(!booleanSample2)
+
+    if (booleanSample || !booleanSample2){
+        println("Statement is true")
+    *//*} else if (booleanSample2){
+        println("Statement is false")*//*
+    } else{
+        println("Statement is false")
+    }*/
+}
---
+++ app/src/main/java/com/android/sample/repo/NewTry.kt
@@ -0,0 +1,10 @@
+package com.android.sample.repo
+
+fun main(){
+    println("Enter your information: ")
+    val name =          readln()
+    val firstName = "Ryan "
+    val lastName = "Malacao"
+    val fullName = "You are $firstName + lastName"
+    println(fullName)
+}
