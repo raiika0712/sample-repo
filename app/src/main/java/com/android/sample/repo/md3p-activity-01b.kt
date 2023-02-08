a5f71d21f55368270c2447179cba4dd8740bbc17
Ryan <kodegomd3p.ryan.malacao@gmail.com>
2023-02-08 23:17:31 +0800

    final act1b

--- app/src/main/java/com/android/sample/repo/md3p-activity-01b.kt
+++ app/src/main/java/com/android/sample/repo/md3p-activity-01b.kt
@@ -1,34 +1,61 @@
 package com.android.sample.repo
 
-fun main(){
-    println("Library List")
-    var stringValue = ""
-    println("Enter book name: ")
-    stringValue = readln()
+fun main() {
 
-    when (stringValue.lowercase()) {
-        "harry potter" -> {
-            println("$stringValue(not available) Genre:Novel, Fictional Fantasy")
+    // book list
+    val book1 = "Harry Potter (AVAILABLE)"
+    val book2 = "Percy Jackson & the Olympians (AVAILABLE)"
+    val book3 = "One Punch Man (AVAILABLE)"
+    val book4 = "Jujutsu Kaisen (AVAILABLE)"
+    val book5 = "Twilight (AVAILABLE)"
+    val book6 = "Highschool DxD (NOT AVAILABLE)"
+
+    println("press ENTER to display the list:")
+    while (true) {
+        val name = readln()
+        if (name.isNullOrBlank()) {
+            println("$book1")
+            println("$book2")
+            println("$book3")
+            println("$book4")
+            println("$book5")
+            println("$book6")
+            println()
+        } else {
+            return (main())
         }
 
-        "percy jackson" -> {
-            println("$stringValue(not available) Genre:Novel, Fictional Fantasy")
-        }
+        var stringValue = ""
+        println("Enter book name: ")
+        stringValue = readln()
 
-        "one punch man" -> {
-            println("$stringValue(available) Genre:Manga, Fictional Action Hero Fantasy")
-        }
+        when (stringValue.lowercase()) {
+            "harry potter" -> {
+                println("$stringValue is a series of seven fantasy novels written by British author J. K. Rowling.")
+            }
 
-        "jujutsu kaisen" -> {
-            println("$stringValue(available) Genre:Manga, Fictional Action Fantasy")
-        }
+            "percy jackson & the olympians" -> {
+                println("$stringValue is a series of five fantasy novels written by American author Rick Riordan, and the first book series in the Camp Half-Blood Chronicles.")
+            }
 
-        "twilight" -> {
-            println("$stringValue(not available) Genre:Novel, Fictional Romance Fantasy")
-        }
+            "one punch man" -> {
+                println("$stringValue (Japanese: ワンパンマン, Hepburn: Wanpanman) is a Japanese superhero manga series created by One.")
+            }
 
-        else -> {
-            println("ERROR: NO DATA ON OUR LIBRARY")
+            "jujutsu kaisen" -> {
+                println("$stringValue (呪術廻戦) is a Japanese shonen manga series written and illustrated by Gege Akutami.")
+            }
+
+            "twilight" -> {
+                println("$stringValue is a series of four fantasy romance novels, two companion novels, and one novella written by American author Stephenie Meyer.")
+            }
+            "highschool dxd" -> {
+                println("$stringValue is NOT AVAILABLE you pervert ;)")
+            }
+
+            else -> {
+                println("ERROR: NO DATA ON OUR LIBRARY")
+            }
         }
     }
 }
